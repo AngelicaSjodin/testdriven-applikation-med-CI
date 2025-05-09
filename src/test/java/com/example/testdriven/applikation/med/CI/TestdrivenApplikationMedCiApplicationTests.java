4cdd0316d46670e549e8b2646dc8878beb205da8
AngelicaSjodin <angelic.sjodin@gmail.com>
2025-05-09 15:14:04 +0200

    first commit

---
+++ src/test/java/com/example/testdriven/applikation/med/CI/TestdrivenApplikationMedCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdriven.applikation.med.CI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdrivenApplikationMedCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
